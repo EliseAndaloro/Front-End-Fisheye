36374726fa93430e867be377157c22fc4f575a90
Elise Andaloro <e-andaloro@sfi.fr>
2022-09-21 15:05:22 +0200

    fetch right domain

--- scripts/services/photographers.service.js
+++ scripts/services/photographers.service.js
@@ -1,30 +1,30 @@
 class PhotographersService {
     fetchAllPhotographers() {
-        return fetch('http://localhost/Front-End-Fisheye/data/photographers.json')
+        return fetch('https://eliseandaloro.github.io/Front-End-Fisheye/data/photographers.json')
         .then(result => result.json())
         .then(datas => datas.photographers);
     }
 
     fetchAllMedia() {
-        return fetch('http://localhost/Front-End-Fisheye/data/photographers.json')
+        return fetch('https://eliseandaloro.github.io/Front-End-Fisheye/data/photographers.json')
         .then(result => result.json())
         .then(datas => datas.media);
     }
 
     fetchPhotographer(id) {
-        return fetch('http://localhost/Front-End-Fisheye/data/photographers.json')
+        return fetch('https://eliseandaloro.github.io/Front-End-Fisheye/data/photographers.json')
             .then(result => result.json())
             .then(datas => datas.photographers.filter(photographer => photographer.id == id));
     }
 
     fetchPhotographerMedia(id) {
-        return fetch('http://localhost/Front-End-Fisheye/data/photographers.json')
+        return fetch('https://eliseandaloro.github.io/Front-End-Fisheye/data/photographers.json')
             .then(result => result.json())
             .then(datas => datas.media.filter(media => media.photographerId == id));
     }
 
     fetchPhotographerMediaSortBy(id, sortBy) {
-        return fetch('http://localhost/Front-End-Fisheye/data/photographers.json')
+        return fetch('https://eliseandaloro.github.io/Front-End-Fisheye/data/photographers.json')
             .then(result => result.json())
             .then( function (datas) {
                 let medias = datas.media.filter(media => media.photographerId == id);
